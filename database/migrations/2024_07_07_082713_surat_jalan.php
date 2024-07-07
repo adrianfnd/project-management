2f99f2b9948b9a92c9be533393cabac199ffbcb3
adrianfnd <fernanda.adrian2@gmail.com>
2024-07-07 08:54:12 +0700

    create table surat jalan + riwayat surat jalan

---
+++ database/migrations/2024_07_07_082713_surat_jalan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surat_jalan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ftth_project_id');
+            $table->foreign('ftth_project_id')->references('id')->on('ftth_projects')->onDelete('cascade');
+            $table->string('nomor_surat');
+            $table->text('deskripsi')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surat_jalan');
+    }
+};
---
+++ database/migrations/2024_07_07_082756_riwayat_surat_jalan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('riwayat_surat_jalan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('surat_jalan_id');
+            $table->foreign('surat_jalan_id')->references('id')->on('surat_jalan')->onDelete('cascade');
+            $table->unsignedBigInteger('ftth_project_id');
+            $table->foreign('ftth_project_id')->references('id')->on('ftth_projects')->onDelete('cascade');
+            $table->text('keterangan');
+            $table->date('tanggal');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('riwayat_surat_jalan');
+    }
+};
