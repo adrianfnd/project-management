aeb6e0cea7b3b52779ab4dacbd5d8600051a5468
adrianfnd <fernanda.adrian2@gmail.com>
2024-07-27 15:24:36 +0700

    create project crud superadmin

--- app/Http/Controllers/superadmin/ProjectController.php
+++ app/Http/Controllers/superadmin/ProjectController.php
@@ -3,100 +3,226 @@
 namespace App\Http\Controllers\superadmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\Sbu;
+use App\Models\Status;
 use App\Models\User;
 use App\Models\Role;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
-class StaffController extends Controller
+class ProjectController extends Controller
 {
     public function index()
     {
-        $page_name = 'Staff';
+        $page_name = 'Projects';
 
-        $staffUsers = User::where('role_id', Role::where('role_name', 'Staff')->first()->id)->paginate(10);
-        
-        return view('superadmin.staff.index', compact('page_name', 'staffUsers'));
+        $projects = Project::with(['type', 'sbu', 'status', 'technician'])->paginate(10);
+
+        return view('superadmin.project.index', compact('page_name', 'projects'));
     }
 
     public function create()
     {
-        $page_name = 'Create Staff';
+        $page_name = 'Create Project';
 
-        return view('superadmin.staff.create', compact('page_name'));
+        $types = Type::all();
+        $sbus = Sbu::all();
+        $statuses = Status::all();
+        $technicians = User::where('role_id', Role::where('role_name', 'TECHNICIAN')->first()->id)->get();
+
+        return view('superadmin.project.create', compact('page_name', 'types', 'sbus', 'statuses', 'technicians'));
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required',
-            'username' => 'required|unique:users',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
+            'type_id' => 'required|exists:types,id',
+            'project_name' => 'required',
+            'olt_hostname' => 'required',
+            'no_sp2k_spa' => 'required',
+            'sbu_id' => 'required|exists:sbus,id',
+            'kendala' => 'nullable',
+            'progress' => 'nullable',
+            'status_id' => 'required|exists:statuses,id',
+            'start_date' => 'required|date',
+            'target' => 'required|date',
+            'end_date' => 'nullable|date',
+            'latitude' => 'nullable',
+            'longitude' => 'nullable',
+            'radius' => 'nullable',
+            'link_file' => 'nullable|file|mimes:pdf|max:10240',
+            'images.*' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'is_active' => 'required|in:Y,N',
+            'technician_id' => 'required|exists:users,id',
         ], [
-            'name.required' => 'Kolom Nama wajib diisi.',
-            'username.required' => 'Kolom Username wajib diisi.',
-            'username.unique' => 'Username sudah digunakan.',
-            'email.required' => 'Kolom Email wajib diisi.',
-            'email.email' => 'Format Email tidak valid.',
-            'email.unique' => 'Email sudah digunakan.',
-            'password.required' => 'Kolom Password wajib diisi.',
-            'password.min' => 'Password minimal harus 6 karakter.',
-        ]);        
+            'type_id.required' => 'Tipe proyek wajib diisi.',
+            'project_name.required' => 'Nama proyek wajib diisi.',
+            'olt_hostname.required' => 'OLT hostname wajib diisi.',
+            'no_sp2k_spa.required' => 'Nomor SP2K/SPA wajib diisi.',
+            'sbu_id.required' => 'SBU wajib diisi.',
+            'status_id.required' => 'Status wajib diisi.',
+            'start_date.required' => 'Tanggal mulai wajib diisi.',
+            'target.required' => 'Tanggal target wajib diisi.',
+            'is_active.required' => 'Status aktif wajib diisi.',
+            'technician_id.required' => 'Teknisi wajib diisi.',
+        ]);
 
-        $user = new User();
-        $user->name = $request->name;
-        $user->username = $request->username;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->role_id = Role::where('role_name', 'Staff')->first()->id;
-        $user->save();
+        $project = new Project();
+        $project->type_id = $request->type_id;
+        $project->project_name = $request->project_name;
+        $project->olt_hostname = $request->olt_hostname;
+        $project->no_sp2k_spa = $request->no_sp2k_spa;
+        $project->sbu_id = $request->sbu_id;
+        $project->kendala = $request->kendala;
+        $project->progress = $request->progress;
+        $project->status_id = $request->status_id;
+        $project->start_date = $request->start_date;
+        $project->target = $request->target;
+        $project->end_date = $request->end_date;
+        $project->latitude = $request->latitude;
+        $project->longitude = $request->longitude;
+        $project->radius = $request->radius;
+        $project->link_file = null;
+        $project->images = null;
+        $project->is_active = $request->is_active;
+        $project->technician_id = $request->technician_id;
+        $project->created_by = auth()->id();
+        $project->save();
 
-        return redirect()->route('staff.index')->with('success', 'Staff berhasil ditambahkan');
+        if ($request->hasFile('link_file')) {
+            $file = $request->file('link_file');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->storeAs('pengajuan' . '/' . $project->project_name . '/', $fileName, 'public');
+        }
+        $imagePaths = [];
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $image) {
+                $path = $image->store('gambar project/' . $project->project_name, 'public');
+                $imagePaths[] = $path;
+            }
+        }
+        $project->update([
+            'link_file' => $project->link_file,
+            'images' => json_encode($imagePaths),
+        ]);
+
+        return redirect()->route('project.index')->with('success', 'Project created successfully');
     }
 
     public function edit($id)
     {
-        $page_name = 'Edit Staff';
+        $page_name = 'Edit Project';
 
-        $user = User::findOrFail($id);
-        
-        return view('superadmin.staff.edit', compact('page_name', 'user'));
+        $project = Project::findOrFail($id);
+        $types = Type::all();
+        $sbus = Sbu::all();
+        $statuses = Status::all();
+        $technicians = User::where('role_id', Role::where('role_name', 'TECHNICIAN')->first()->id)->get();
+
+        return view('superadmin.project.edit', compact('page_name', 'project', 'types', 'sbus', 'statuses', 'technicians'));
     }
-
     public function update(Request $request, $id)
     {
         $request->validate([
-            'name' => 'required',
-            'username' => 'required|unique:users,username,'.$id,
-            'email' => 'required|email|unique:users,email,'.$id,
+            'type_id' => 'required|exists:types,id',
+            'project_name' => 'required',
+            'olt_hostname' => 'required',
+            'no_sp2k_spa' => 'required',
+            'sbu_id' => 'required|exists:sbus,id',
+            'kendala' => 'nullable',
+            'progress' => 'nullable',
+            'status_id' => 'required|exists:statuses,id',
+            'start_date' => 'required|date',
+            'target' => 'required|date',
+            'end_date' => 'nullable|date',
+            'latitude' => 'nullable',
+            'longitude' => 'nullable',
+            'radius' => 'nullable',
+            'link_file' => 'nullable|file|mimes:pdf|max:10240',
+            'images.*' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'is_active' => 'required|in:Y,N',
+            'technician_id' => 'required|exists:users,id',
         ], [
-            'name.required' => 'Kolom Nama wajib diisi.',
-            'username.required' => 'Kolom Username wajib diisi.',
-            'username.unique' => 'Username sudah digunakan.',
-            'email.required' => 'Kolom Email wajib diisi.',
-            'email.email' => 'Format Email tidak valid.',
-            'email.unique' => 'Email sudah digunakan.',
+            'type_id.required' => 'Tipe proyek wajib diisi.',
+            'project_name.required' => 'Nama proyek wajib diisi.',
+            'olt_hostname.required' => 'OLT hostname wajib diisi.',
+            'no_sp2k_spa.required' => 'Nomor SP2K/SPA wajib diisi.',
+            'sbu_id.required' => 'SBU wajib diisi.',
+            'status_id.required' => 'Status wajib diisi.',
+            'start_date.required' => 'Tanggal mulai wajib diisi.',
+            'target.required' => 'Tanggal target wajib diisi.',
+            'is_active.required' => 'Status aktif wajib diisi.',
+            'technician_id.required' => 'Teknisi wajib diisi.',
         ]);
-
-        $user = User::findOrFail($id);
-        $user->name = $request->name;
-        $user->username = $request->username;
-        $user->email = $request->email;
-        if ($request->filled('password')) {
-            $user->password = Hash::make($request->password);
+    
+        $project = Project::findOrFail($id);
+        $project->type_id = $request->type_id;
+        $project->project_name = $request->project_name;
+        $project->olt_hostname = $request->olt_hostname;
+        $project->no_sp2k_spa = $request->no_sp2k_spa;
+        $project->sbu_id = $request->sbu_id;
+        $project->kendala = $request->kendala;
+        $project->progress = $request->progress;
+        $project->status_id = $request->status_id;
+        $project->start_date = $request->start_date;
+        $project->target = $request->target;
+        $project->end_date = $request->end_date;
+        $project->latitude = $request->latitude;
+        $project->longitude = $request->longitude;
+        $project->radius = $request->radius;
+        $project->is_active = $request->is_active;
+        $project->technician_id = $request->technician_id;
+        $project->updated_by = auth()->id();
+    
+        if ($request->hasFile('link_file')) {
+            if ($project->link_file) {
+                Storage::disk('public')->delete($project->link_file);
+            }
+            $file = $request->file('link_file');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->storeAs('pengajuan' . '/' . $project->project_name . '/', $fileName, 'public');
+            $project->link_file = $filePath;
         }
-        $user->save();
-
-        return redirect()->route('staff.index')->with('success', 'Staff berhasil diubah');
+    
+        if ($request->hasFile('images')) {
+            if ($project->images) {
+                $oldImages = json_decode($project->images, true);
+                foreach ($oldImages as $oldImage) {
+                    Storage::disk('public')->delete($oldImage);
+                }
+            }
+            $imagePaths = [];
+            foreach ($request->file('images') as $image) {
+                $path = $image->store('gambar project/' . $project->project_name, 'public');
+                $imagePaths[] = $path;
+            }
+            $project->images = json_encode($imagePaths);
+        }
+    
+        $project->save();
+    
+        return redirect()->route('project.index')->with('success', 'Project updated successfully');
     }
-
+    
     public function destroy($id)
     {
-        $user = User::findOrFail($id);
+        $project = Project::findOrFail($id);
 
-        $user->delete();
-
-        return redirect()->route('staff.index')->with('success', 'Staff berhasil dihapus');
+        if ($project->link_file) {
+            Storage::disk('public')->delete($project->link_file);
+        }
+    
+        if ($project->images) {
+            $images = json_decode($project->images, true);
+            foreach ($images as $image) {
+                Storage::disk('public')->delete($image);
+            }
+        }
+    
+        $project->delete();
+    
+        return redirect()->route('project.index')->with('success', 'Project deleted successfully');
     }
 }
---
+++ resources/views/superadmin/project/index.blade.php
@@ -0,0 +1,156 @@
+@extends('main')
+
+@section('content')
+    <div class="container-fluid py-4">
+        <div class="row mb-4">
+            <div class="col-12">
+                <div class="card mb-4">
+                    <div class="card-header pb-0">
+                        <h6>Projects table</h6>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="d-flex justify-content-between align-items-center px-4 pb-3">
+                            <div></div>
+                            <a href="{{ route('project.create') }}" class="btn btn-sm btn-primary" role="button"
+                                data-bs-toggle="tooltip" data-bs-placement="top" title="Create project">
+                                <i class="fa fa-plus" style="margin-right: 5px; vertical-align: middle;"></i>
+                                <span style="vertical-align: middle;">Create Project</span>
+                            </a>
+                        </div>
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center justify-content-center mb-0">
+                                <thead>
+                                    <tr>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            No.</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Project Name</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Type</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            SBU</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Status</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Start</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Target</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($projects as $index => $project)
+                                        <tr>
+                                            <td class="align-middle text-center">
+                                                <span
+                                                    class="text-secondary text-xs font-weight-bold">{{ $index + 1 }}</span>
+                                            </td>
+                                            <td>
+                                                <div class="d-flex px-2">
+                                                    <div class="my-auto">
+                                                        <h6 class="mb-0 text-sm">{{ $project->project_name }}</h6>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <p class="text-sm font-weight-bold mb-0">{{ $project->type->type_name }}</p>
+                                            </td>
+                                            <td class="align-middle text-center text-sm">
+                                                <span
+                                                    class="text-secondary text-xs font-weight-bold">{{ $project->sbu->sbu_name }}</span>
+                                            </td>
+                                            <td class="align-middle text-center text-sm">
+                                                <span
+                                                    class="text-secondary text-xs font-weight-bold">{{ $project->status->status_name }}</span>
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <span
+                                                    class="text-secondary text-xs font-weight-bold">{{ $project->start_date }}</span>
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <span
+                                                    class="text-secondary text-xs font-weight-bold">{{ $project->target }}</span>
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <a href="{{ route('project.edit', $project->id) }}"
+                                                    class="btn btn-xs btn-primary btn-sm" role="button"
+                                                    data-bs-toggle="tooltip" data-bs-placement="top" title="Edit project">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <button type="submit" class="btn btn-xs btn-danger btn-sm"
+                                                    onclick="confirmDelete({{ $project->id }})" data-bs-toggle="tooltip"
+                                                    data-bs-placement="top" title="Delete project">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                                <form id="delete-form-{{ $project->id }}"
+                                                    action="{{ route('project.destroy', $project->id) }}" method="POST"
+                                                    style="display: inline-block;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- Pagination links -->
+                <div class="d-flex justify-content-end mt-4">
+                    {{ $projects->links('pagination::bootstrap-4') }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        function confirmDelete(projectId) {
+            Swal.fire({
+                title: 'Apakah anda yakin?',
+                text: "Anda tidak akan dapat mengembalikan ini!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Iya, hapus!',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('delete-form-' + projectId).submit();
+                }
+            })
+        }
+
+        var successMessage = '{{ session('success') }}';
+        var errorMessage = '{{ session('error') }}';
+        if (successMessage) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: successMessage,
+                showConfirmButton: false,
+                timer: 2000
+            });
+        }
+
+        if (errorMessage) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal!',
+                text: errorMessage,
+                showConfirmButton: false,
+                timer: 2000
+            });
+        }
+    </script>
+@endsection
